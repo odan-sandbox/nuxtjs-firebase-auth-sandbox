4eba8fe07e5500e68f19004a3913cdd04efc6501
odan <odan3240@gmail.com>
2020-07-21 10:15:55 +0900

    add todo

--- middleware/authenticated.ts
+++ middleware/authenticated.ts
@@ -2,6 +2,7 @@ import { Middleware } from '@nuxt/types'
 
 const middleware: Middleware = ({ req, redirect, beforeNuxtRender }) => {
   if (process.client) {
+    // TODO: validate frontend
     return
   }
   const user = req.user
